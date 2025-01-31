f1241eaabf165fdd3adb4aad919f0c9fdf682c62
kisudevel <kisudevel@gmail.com>
2025-01-31 17:50:36 +0900

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onBeforeMount, ref } from "vue";
+
+const posts = ref(null);
+onBeforeMount(() => {
+  fetch(`${import.meta.env.VITE_API_URL}/posts`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      posts.value = data;
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+});
+</script>
+<template>
+  <h1>About</h1>
+  <pre>{{ posts }}</pre>
+</template>
+<style scoped></style>
